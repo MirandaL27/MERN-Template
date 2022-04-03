7347d4719898413f1052e3d4a2f2d680bb97c9d9
Cadenza <mirandalucsb@gmail.com>
2022-04-02 17:41:12 -0700

    fixed typo

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/<your database name', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/<your database name>', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
